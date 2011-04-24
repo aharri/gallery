0b4f0d62be103c6bbe76e8c0f9d113ba47e7ff32
Antti Harri <iku@openbsd.fi>
2011-04-24 03:12:18 +0300

    Bump minor to version 1.5 due to several fixes.

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ require_once('libs/class.validate.php');
 require_once('libs/class.acms_xml.php');
 require_once('libs/imagelibs/class.image.php');
 
-define ('VERSION', '1.4');
+define ('VERSION', '1.5');
 define ('AUTHOR', 'Antti Harri <iku@openbsd.fi>');
 
 class pager
