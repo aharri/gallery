04c75f239be4bb83bc660bde8322c083d84d1302
Antti Harri <iku@openbsd.fi>
2011-06-26 22:00:16 +0300

    Bump copyright year.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (c) 2007 Antti Harri <iku@openbsd.fi>
+ * Copyright (c) 2007,2011 Antti Harri <iku@openbsd.fi>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
