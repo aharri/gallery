f53e29be6e1d361763b3c357cb51adf44eda5af3
Antti Harri <iku@openbsd.fi>
2016-02-23 08:48:03 +0200

    Fix typo in the range code. By Andrew Waters on the original site.

--- wrapper.php
+++ wrapper.php
@@ -72,7 +72,7 @@ function rangeDownload($file) {
 		// If the range starts with an '-' we start from the beginning
 		// If not, we forward the file pointer
 		// And make sure to get the end byte if spesified
-		if ($range0 == '-') {
+		if ($range == '-') {
  
 			// The n-number of the last bytes is requested
 			$c_start = $size - substr($range, 1);
